5fe9a4449c61921d691188fbf757cd88077e3b96
Giordano Rossiello <kloon247@gmail.com>
2020-04-11 21:31:31 +0200

    With this update, my file contains a possible solution to both the requested algorithms. We need to discuss whether this is the correct approach or not, and modify the file accordingly.

--- Homework1/G30HW1_Giordano.py
+++ Homework1/G30HW1_Giordano.py
@@ -3,9 +3,8 @@ import sys
 import os
 
 
-def class_count_1(pairs, K):
-
-    def map_pair_phase_1(pair):
+def class_count(pairs, K):
+    def map_pair_phase(pair):
         pair_split = pair.split(' ', 1)
         return [(int(pair_split[0]) % K, pair_split[1])]
 
@@ -18,35 +17,39 @@ def class_count_1(pairs, K):
                 pairs_dict[c] += 1
         return [(key, pairs_dict[key]) for key in pairs_dict.keys()]
 
-    class_count = (pairs.flatMap(map_pair_phase_1)  # <-- MAP PHASE (R1)
+    class_count = (pairs.flatMap(map_pair_phase)  # <-- MAP PHASE (R1)
                    .groupByKey()  # <-- REDUCE PHASE (R1)
                    .flatMap(gather_pairs)
                    .reduceByKey(lambda x, y: x + y))  # <-- REDUCE PHASE (R2)
     return class_count
 
 
-def class_count_1_with_partition(docs):
-
-    def map_pair_phase_1(pair):
+def class_count_with_partition(pairs):
+    def map_pair_phase(pair):
         pair_split = pair.split(' ', 1)
         return [(int(pair_split[0]), pair_split[1])]
 
     def gather_pairs_partitions(pairs):
         pairs_dict = {}
+        i = 0
         for p in pairs:
-            word, occurrences = p[0], p[1]
-            if word not in pairs_dict.keys():
-                pairs_dict[word] = occurrences
+            i += 1
+            c = p[1]
+            if c not in pairs_dict.keys():
+                pairs_dict[c] = 1
             else:
-                pairs_dict[word] += occurrences
+                pairs_dict[c] += 1
+        pairs_dict["maxPartitionSize"] = i
         return [(key, pairs_dict[key]) for key in pairs_dict.keys()]
 
-    word_count = (docs.flatMap(map_pair_phase_1)  # <-- MAP PHASE (R1)
+    class_init = (pairs.flatMap(map_pair_phase)  # <-- MAP PHASE (R1)
                   .mapPartitions(gather_pairs_partitions)  # <-- REDUCE PHASE (R1)
-                  .groupByKey()  # <-- REDUCE PHASE (R2)
-                  .mapValues(lambda vals: sum(vals)))
+                  .groupByKey())  # <-- REDUCE PHASE (R2)
 
-    return word_count
+    class_count = class_init.filter(lambda x: x[0] != "maxPartitionSize").mapValues(sum)
+    max_partition_size = class_init.filter(lambda x: x[0] == "maxPartitionSize").mapValues(max)
+
+    return class_count.union(max_partition_size)
 
 
 def main():
@@ -86,21 +89,22 @@ def main():
     # CLASS COUNT with groupByKey
     # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-    cc = class_count_1(pair_strings, K)
+    cc = class_count(pair_strings, K)
     print("VERSION WITH DETERMINISTIC PARTITIONS")
-    print("Output Pairs = ", ' '.join(map(lambda pair: "({},{})".format(pair[0], str(pair[1])), cc.sortByKey().collect())))
+    print("Output Pairs = ", ' '.join(map(lambda pair: f"({pair[0]},{pair[1]})", cc.sortByKey().collect())))
 
     # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-    # IMPROVED WORD COUNT with mapPartitions
+    # CLASS COUNT with mapPartitions
     # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-    # wc = word_count_2_with_partition(pair_strings)
-    # print("Number of distinct words in the documents = ", wc.count())
-
-    # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-    # COMPUTE AVERAGE WORD LENGTH
-    # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-    # average_word_len = wc.keys().map(lambda x: (x, len(x))).values().mean()
-    # print("Average word length = ", average_word_len)
+    cc = class_count_with_partition(pair_strings)
+    # The map function, in case of ties, picks the first maximum value
+    # Sorting by key before searching the maximum guarantees that the map function will pick the smaller class in
+    # alphabetical order
+    max_count = cc.sortByKey().filter(lambda x: x[0] != "maxPartitionSize").max(lambda pair: pair[1])
+    max_partition_size = cc.filter(lambda x: x[0] == "maxPartitionSize").collect()
+    print("VERSION WITH SPARK PARTITIONS")
+    print(f"Most frequent class = ({max_count[0]},{max_count[1]})")
+    print(f"Max partition size = {max_partition_size[0][1]}")
 
 
 if __name__ == "__main__":
