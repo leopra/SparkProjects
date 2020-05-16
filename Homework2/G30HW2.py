4ff4e5c9e9bb18336483f48840a2c8fc583cd05c
Giordano Rossiello <kloon247@gmail.com>
2020-05-16 20:23:16 +0200

    Merge completed! I ran the entire code, and it returns reasonable results. Now, please take some time to comment your parts of code, if you feel the need to explain the reasoning beyond what you wrote. I'll upload my comments later. Remember, the soft deadline expires tomorrow. Let's finish this so that we don't lose the extra point! :D Be careful though, always pull first, insert comments and finally push when you are confident other people is not doing the same. This way, we don't collide with our updates, and we don't lose the comments.

--- Homework2/G30HW2.py
+++ Homework2/G30HW2.py
@@ -9,34 +9,40 @@ import TupleInput as ti
 import numpy as np
 import time 
 
+def euclidean_distance(u, v):
+    return np.sqrt(sum([(a - b) ** 2 for a, b in zip(u, v)]))
+
 #receives in input a set of points S and returns the max distance between two 
 # points in S.
 def exactMPD(S):
-    return 0
+    max_dist = 0
+
+    for i in range(0, len(S) - 1):
+        for j in range(i + 1, len(S)):
+            max_dist = max(euclidean_distance(S[i], S[j]), max_dist)
+
+    return max_dist
 
 #receives in input a set of points S and an interger k < |S|, selects k points 
 # at random from S (let S' denote the set of these k points) and returns the 
 # maximum distance d(x,y), over all x in S' and y in S. 
 # Define a constant SEED in your main program 
 
-def twoApproxMPD(S,k):
+def twoApproxMPD(S, k):
+    assert k < len(S), "k must be less than the size of S"
 
-    assert k < len(S)
-    co1=[]
-    maxds=0
-    L = len(S)-1
-    kpoints = rand.sample(range(1,L), k)
+    co_1 = []
+    max_ds = 0
+    k_points = rand.sample(range(0, len(S)), k)
 
-    for i in kpoints:
-        co1.append(S[i])
+    for i in k_points:
+        co_1.append(S[i])
 
     for co in S:
-        for point in co1:
-            temp = np.sqrt(sum([(x-z)**2 for x, z in zip(co,point)]))
-        if temp > maxds:
-            maxds = temp
+        for point in co_1:
+            max_ds = max(euclidean_distance(co, point), max_ds)
 
-    return maxds
+    return max_ds
 
  
 
@@ -44,22 +50,68 @@ def twoApproxMPD(S,k):
 # of k centers selected from S using the Farthest-First Traversal algorithm. 
 # It is important that kCenterMPD(S,k) run in O(|S|*k) time
 def kCenterMPD(S,k):
-    return 0
+    assert k < len(S), "k must be less than the size of S"
+
+    P = set(S)
+    S = set()
+
+    p = P.pop()
+    S.add(p)
+    dists_dict = {key: euclidean_distance(key, p) for key in P}
+
+    for i in range(1, k):
+        c = max(dists_dict, key=dists_dict.get)
+        for t in P:
+            dists_dict[t] = min(dists_dict[t], euclidean_distance(t, c))
+        S.add(c)
+        P.discard(c)
+
+    return list(S)
 
 
 def main():
     rand.seed(42)
 
-    assert len(sys.argv) == 3
+    assert len(sys.argv) == 3, "Two arguments must be provided: k desired number of centers, S set of points"
+    assert str(sys.argv[1]).isdigit(), "k must be a number"
 
     # SPARK SETUP
     #conf = SparkConf().setAppName('HW2').setMaster("local[*]")
     #sc = SparkContext(conf=conf)
 
-    data = ti.readTuplesSeq(sys.argv[2])
-    s = time.time()
-    print('twoApproxMPD: ', twoApproxMPD(data, int(sys.argv[1])))
-    e = time.time()
-    print('time taken: ', e-s)
+    inputPoints = ti.readTuplesSeq(sys.argv[2])
+    k = int(sys.argv[1])
+
+    start = time.time()
+    result = exactMPD(inputPoints)
+    elapsed = time.time() - start
+    print('EXACT ALGORITHM')
+    print(f'Max distance = {result}')
+    print(f'Running time = {elapsed * 1000}')
+
+    print('')
+
+    start = time.time()
+    result = twoApproxMPD(inputPoints, k)
+    elapsed = time.time() - start
+
+    print('2-APPROXIMATION ALGORITHM')
+    print(f'k = {k}')
+    print(f'Max distance = {result}')
+    print(f'Running time = {elapsed * 1000}')
+
+    print('')
+
+    start = time.time()
+    centers = kCenterMPD(inputPoints, k)
+    result = exactMPD(centers)
+    elapsed = time.time() - start
+
+    print('k-CENTER-BASED ALGORITHM')
+    print(f'k = {k}')
+    print(f'Max distance = {result}')
+    print(f'Running time = {elapsed * 1000}')
+
+
 if __name__ == "__main__":
     main()
