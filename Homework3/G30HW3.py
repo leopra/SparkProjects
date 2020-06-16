3e0fbfb31a024b7d0b1f273bedab058092e55167
Example <example@example.com>
2020-06-17 01:02:23 +0200

    Changed the print commands to easy use in Cloud

--- Homework3/G30HW3.py
+++ Homework3/G30HW3.py
@@ -1,21 +1,13 @@
-#this part needed just for me Leo
-#import findspark
-#findspark.init('/home/leokane96/SparkDir/spark-3.0.0-preview2-bin-hadoop2.7')
 from pyspark import SparkContext, SparkConf
-
 import sys
 import os
 import random as rand
-
 import numpy as np
 import time 
-
 from runSequential import runSequential
-
 
 def euclidean_distance(u, v):
     return np.sqrt(sum([(a - b) ** 2 for a, b in zip(u, v)]))
-
 
 def kCenterMPD(S, k):
     S = list(S)
@@ -32,6 +24,7 @@ def kCenterMPD(S, k):
     # in the following for loop, the dict is updated once for every new center added to the resulting set,
     # keeping the minimum between the current distance and the computed distance from the new center.
     # This should ensure that the entire method has a complexity of O(k*|S|)
+
     dists_dict = {key: euclidean_distance(key, p) for key in P}
 
     for i in range(1, k):  # O(k)
@@ -41,17 +34,11 @@ def kCenterMPD(S, k):
         S.add(c)
         P.discard(c)
 
-    # TOT: O(k * (|S| + |S|)) = O(k*|S|)
-
     return list(S)
-    # else:
-    #     return 0
-
 
 def callKcenter(S):
     global K
     return kCenterMPD(S, K)
-
 
 #(a) runMapReduce(pointsRDD,k,L): implements the 4-approximation 
 # MapReduce algorithm for diversity maximization described above. More specifically, 
@@ -75,22 +62,18 @@ def runMapReduce(pointsRDD,k,L):
 
     return solution, r1_elapsed, r2_elapsed
 
-
 #(b) measure(pointsSet): receives in input a set of points (pointSet) and computes 
 # the average distance between all pairs of points. The set pointSet must be 
 # represented as ArrayList<Vector> (in Java) or list of tuple (in Python).
 
 def measure(pointSet):
-    # pairsDistance = [euclidean_distance(pointSet[i], pointSet[j]) for i in range(len(pointSet)) for j in range(i+1, len(pointSet))]
-    #
-    # return np.mean(pairsDistance)
+
     tot = 0.
     for i in range(len(pointSet) - 1):
         for j in range(i + 1, len(pointSet)):
             tot += euclidean_distance(pointSet[i], pointSet[j])
 
     return tot / ((len(pointSet) * (len(pointSet) - 1)) / 2)
-
 
 K = 0
 
@@ -104,29 +87,26 @@ def main():
     inputPath = sys.argv[1]
     k = int(sys.argv[2])
     # SPARK SETUP
-    conf = SparkConf().setAppName('HW3')  # .setMaster("local[*]") not needed
+    conf = SparkConf().setAppName('HW3')
     sc = SparkContext(conf=conf)
 
     # to read the input and convert tuple of string to tuple of float
     start = time.time()
     docs = sc.textFile(inputPath).map(lambda x: tuple(float(dim) for dim in x.split(","))).repartition(L).cache()
-    # docs = sc.textFile(inputPath).map(lambda x: tuple(float(dim) for dim in x.split(","))).repartition(L).collect()
     init_time = time.time() - start
 
-    print(f"Number of points = {docs.count()}")
-    print(f"k = {k}")
-    print(f"L = {L}")
-    print(f"Initialization time = {init_time * 1000} ms")
+    print("Number of points =" , docs.count())
+    print("k = ", k)
+    print("L = ", L)
+    print("Initialization time = ", init_time * 1000)
 
     solution, r1_time, r2_time = runMapReduce(docs, k, L)
 
-    print(f"Runtime of Round 1: {r1_time * 1000} ms")
-    print(f"Runtime of Round 2: {r2_time * 1000} ms")
+    print("Runtime of Round 1 = ", r1_time * 1000)
+    print("Runtime of Round 2 = ", r2_time * 1000)
 
     avg = measure(solution)
-
-    print(f"Average distance = {avg}")
-
+    print("Average distance = ", avg)
 
 if __name__ == "__main__":
     main()
